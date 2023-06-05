8ff07652acda55b222ba85275e7af28f8814bfd7
woai3c <411020382@qq.com>
2023-06-05 17:26:49 +0800

    feat: 升级代码自动格式方案

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,30 +1,28 @@
 module.exports = {
     extends: [
         'stylelint-config-standard-scss',
-        'stylelint-config-recommended-vue'
-    ],
-    customSyntax: 'postcss-html',
-    overrides: [
-        {
-            files: ['**/*.{scss,css,sass}'], // css 相关文件由 postcss-scss 处理
-            customSyntax: 'postcss-scss'
-        },
     ],
     rules: {
-        'indentation': 4,
-        'selector-pseudo-element-no-unknown': [
-            true,
-            {
-                ignorePseudoElements: ['v-deep']
-            }
-        ],
-        'number-leading-zero': 'never',
+        indentation: 4,
+        'media-feature-range-notation': null,
+        'alpha-value-notation': ['number'],
+        'color-function-notation': ['legacy'],
         'no-descending-specificity': null,
         'font-family-no-missing-generic-family-keyword': null,
         'selector-type-no-unknown': null,
         'at-rule-no-unknown': null,
         'no-duplicate-selectors': null,
-        'no-empty-source':null,
-        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }]
-    }
-}
+        'no-empty-source': null,
+        'selector-class-pattern': null,
+        'selector-pseudo-class-no-unknown': [
+            true,
+            { ignorePseudoClasses: ['global', 'deep'] },
+        ],
+        'scss/at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: ['tailwind'],
+            },
+        ],
+    },
+};
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,9 +19,7 @@ const count = ref(0)
     <p>See <code>README.md</code> for more information.</p>
 
     <p>
-        <a href="https://vitejs.dev/guide/features.html" target="_blank">
-            Vite Docs
-        </a>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
         |
         <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
     </p>
@@ -39,7 +37,7 @@ a {
 }
 
 label {
-    margin: 0 .5em;
+    margin: 0 0.5em;
     font-weight: bold;
 }
 
